2687a07fa26802a33088b0ff707142665187c2bc
lutfiadnan92 <adnanlutfi92@gmail.com>
2023-07-01 00:38:56 +0700

    docs: update var description

---
+++ variable/var.js
@@ -0,0 +1,18 @@
+// var
+// The var statement declares a function-scoped or globally-scoped variable, 
+// optionally initializing it to a value.
+
+// example
+var a = 1
+console.log(a) // output: 1
+
+// var declarations, wherever they occurs, are processed before any code is executed.
+// this is called hoisting
+// Duplicate variable declarations using var will not trigger an error,
+// even in strict mode, and the variable will not lose its value, unless another assignment is performed.
+
+// example
+b
+console.log(b) // output: undefined
+var b = 1
+console.log(b) // output: 1
